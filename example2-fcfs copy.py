e551a9dd9cb532a25b9b25279fc3cab513c9642e
gemilang <git.gemilang.santiyuda@gmail.com>
2023-04-13 13:03:57 +0800

    add ebf tutorial

---
+++ example2-fcfs copy.py
@@ -0,0 +1,20 @@
+from batsim_py import SimulatorHandler
+from fcfs import FCFSScheduler
+
+if __name__ == "__main__":
+    workloads_path = "workloads/fcfs.json"
+    platform_path = "platform/platform-8-host.xml"
+    simulator = SimulatorHandler()
+    simulator.start(platform=platform_path, workload=workloads_path)
+    scheduler = FCFSScheduler(simulator)
+    while simulator.is_running:
+        print("+++++ TIME:", simulator.current_time)
+        print("QUEUE before assignment:",simulator.queue)
+        scheduler.schedule()
+        print("QUEUE after assignment:",simulator.queue)
+        print("KEADAAN HOST:\n-----------------------")
+        for host in simulator.platform.hosts:
+            print(host,host.jobs)
+        print("-----------------------")
+        simulator.proceed_time(10)
+    simulator.close()
--- example3-ebf.py
+++ example3-ebf.py
@@ -1,15 +1,20 @@
 from batsim_py import SimulatorHandler
-from fcfs import FCFSScheduler
+from easy_backfilling import EASYScheduler
 
 if __name__ == "__main__":
-    workloads_path = "workloads/simple-8-host.json"
+    workloads_path = "workloads/easybf.json"
     platform_path = "platform/platform-8-host.xml"
     simulator = SimulatorHandler()
     simulator.start(platform=platform_path, workload=workloads_path)
-    scheduler = FCFSScheduler(simulator)
+    scheduler = EASYScheduler(simulator)
     while simulator.is_running:
+        print("+++++ TIME:", simulator.current_time)
         print("QUEUE before assignment:",simulator.queue)
         scheduler.schedule()
         print("QUEUE after assignment:",simulator.queue)
-        simulator.proceed_time(10)
+        print("KEADAAN HOST:\n-----------------------")
+        for host in simulator.platform.hosts:
+            print(host,host.jobs)
+        print("-----------------------")
+        simulator.proceed_time()
     simulator.close()
